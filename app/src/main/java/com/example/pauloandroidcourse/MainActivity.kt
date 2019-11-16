651838f2b483d92d2c1942ad734b957764351466
omar jakmira <poweromar19942@gmail.com>
2019-11-16 16:48:24 +0200

    seekbar

--- app/src/main/java/com/example/pauloandroidcourse/MainActivity.kt
+++ app/src/main/java/com/example/pauloandroidcourse/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.pauloandroidcourse
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,17 +14,45 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        media = MediaPlayer.create(applicationContext,R.raw.watch_me)
+        media = MediaPlayer.create(applicationContext, R.raw.watch_me)
+        /*try {
+            media = MediaPlayer()
+            media?.setDataSource("http://buildappswithpaulo.com/music/watch_me.mp3")
+            media?.prepareAsync()
+            media?.setOnPreparedListener {
+                it.start()
+            }
+        }catch (e:Exception){
+            Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
+        }*/
+
+
+        media?.setOnCompletionListener {
+            Toast.makeText(this@MainActivity, (it.duration / 1000) / 60, Toast.LENGTH_SHORT).show()
+        }
+        seekBarId.max = media!!.duration
+
+        seekBarId.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                if (p2)
+                    media?.seekTo(p1)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
         var counter = 0
         playButton.setOnClickListener {
-            if(counter == 0)
-            {
+            if (counter == 0) {
                 media?.start()
                 counter = 1
                 playButton.text = "Pause"
-            }
-            else
-            {
+            } else {
                 media?.pause()
                 counter = 0
                 playButton.text = "play"
