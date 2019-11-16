f6bd07dec5b7562faef432c94f33eb8f068fce6b
omar jakmira <poweromar19942@gmail.com>
2019-11-16 12:17:45 +0200

    simpler approach

--- app/src/main/java/com/example/pauloandroidcourse/MainActivity.kt
+++ app/src/main/java/com/example/pauloandroidcourse/MainActivity.kt
@@ -1,38 +1,45 @@
 package com.example.pauloandroidcourse
 
-import android.Manifest
-import android.app.Activity
-import android.app.AlertDialog
-import android.content.DialogInterface
+
+import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.GoogleApiAvailability
-import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.location.*
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
-    GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-    private var locationRequest: LocationRequest? = null
-    lateinit var client: GoogleApiClient
-    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    var permissionsToRequest = ArrayList<String>()
-    var permissions = ArrayList<String>()
-    var permissionsRejected = ArrayList<String>()
+class MainActivity : AppCompatActivity() {
 
-    companion object {
-        val UPDATE_INTERVAL: Long = 5000
-        val FASTEST_INTERVAL: Long = 5000
+    var locationManager: LocationManager? = null
+    var locationListener: LocationListener? = null
+
+    var listener = object : LocationListener {
+        override fun onLocationChanged(location: Location?) {
+            Log.d("lc", location.toString())
+            locationTxt.text = "${location?.latitude},${location?.longitude}"
+        }
+
+        override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+
+        }
+
+        override fun onProviderEnabled(p0: String?) {
+
+        }
+
+        override fun onProviderDisabled(p0: String?) {
+
+        }
+
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
@@ -40,189 +47,64 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fusedLocationProviderClient =
-            LocationServices.getFusedLocationProviderClient(this@MainActivity)
-
-        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
-        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION)
-
-        permissionsToRequest = permissionsToRequest(permissions)
-        if (permissionsToRequest.size > 0){
-            requestPermissions(permissionsToRequest.toTypedArray(),1111)
-        }
-
-        client = GoogleApiClient.Builder(this@MainActivity)
-            .addApi(LocationServices.API)
-            .addOnConnectionFailedListener(this@MainActivity)
-            .addConnectionCallbacks(this@MainActivity)
-            .build()
-    }
-
-
-    fun permissionsToRequest(permission: ArrayList<String>): ArrayList<String> {
-        var results = ArrayList<String>()
-
-        for (i in 0 until permission.size) {
-            if (!hasPermission(permission[i])) {
-                results.add(permission[i])
-            }
-        }
-        return results
-    }
-
-    fun hasPermission(permission: String): Boolean {
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
-            return checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
-        }
-        return true
-    }
-
-
-    override fun onStart() {
-        super.onStart()
-        client?.connect()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        client?.disconnect()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if (client?.isConnected) {
-            LocationServices.getFusedLocationProviderClient(this@MainActivity)
-                .removeLocationUpdates(object : LocationCallback() {})
-            client?.disconnect()
-        }
-    }
-
-    fun startLocationUpdate() {
-        locationRequest = LocationRequest()
-        locationRequest?.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest?.interval = UPDATE_INTERVAL
-        locationRequest?.fastestInterval = FASTEST_INTERVAL
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationListener = listener
         if (ActivityCompat.checkSelfPermission(
                 this@MainActivity,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED &&
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED &&
             ActivityCompat.checkSelfPermission(
                 this@MainActivity,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
         ) {
-            Toast.makeText(this@MainActivity, "Permission denied for location", Toast.LENGTH_SHORT)
-                .show()
+            ActivityCompat.requestPermissions(
+                this@MainActivity, arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ), 1111
+            );
+            return
+        }else{
+            locationManager?.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0,
+                0f,
+                locationListener
+            )
         }
 
-        LocationServices.getFusedLocationProviderClient(this@MainActivity)
-            .requestLocationUpdates(locationRequest, object : LocationCallback() {
-                override fun onLocationResult(locationResult: LocationResult?) {
-                    super.onLocationResult(locationResult)
-                    var location = locationResult?.lastLocation
-                    locationTxt.text = "Updated ${location?.latitude},${location?.longitude}"
-                }
 
-                override fun onLocationAvailability(p0: LocationAvailability?) {
-                    super.onLocationAvailability(p0)
-                }
-            }, null)
+
+
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            1111 -> {
-                for (perm in permissionsToRequest) {
-                    if (!hasPermission(perm)) {
-                        permissionsRejected.add(perm)
-                    }
-                }
-
-                if (permissionsRejected.size > 0) {
-                    if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
-                        AlertDialog.Builder(this@MainActivity)
-                            .setMessage("These permissions are Mandatory to get location")
-                            .setPositiveButton("Ok") { p0, p1 ->
-                                requestPermissions(permissionsRejected.toTypedArray(),1111)
-                            }
-                            .setNegativeButton("Cancel", null)
-                            .create()
-                            .show()
-                    }
-                } else {
-                    client?.connect()
-                }
-            }
-        }
-    }
-
-    override fun onPostResume() {
-        super.onPostResume()
-        var errorCode = GoogleApiAvailability.getInstance()
-            .isGooglePlayServicesAvailable(this@MainActivity)
-        if (errorCode != ConnectionResult.SUCCESS) {
-            var errorDialog = GoogleApiAvailability.getInstance()
-                .getErrorDialog(
+        if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(
                     this@MainActivity,
-                    errorCode,
-                    errorCode,
-                    object : DialogInterface.OnCancelListener {
-                        override fun onCancel(p0: DialogInterface?) {
-                            Toast.makeText(
-                                this@MainActivity,
-                                "Location service Error",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-
-                    })
-            errorDialog.show()
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                locationManager?.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    0,
+                    0f,
+                    locationListener
+                )
+            }
         }
     }
 
-    override fun onProviderDisabled(p0: String?) {
-    }
-
-    override fun onProviderEnabled(p0: String?) {
-    }
-
-    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
-    }
-
-    override fun onLocationChanged(location: Location?) {
-        locationTxt.text = "Location Changed ${location?.latitude},${location?.longitude}"
-    }
-
-    override fun onConnected(bundle: Bundle?) {
-        if (ActivityCompat.checkSelfPermission(
-                this@MainActivity,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                this@MainActivity,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        )
-            return
-
-        fusedLocationProviderClient.lastLocation
-            .addOnSuccessListener {
-                Log.d("Location321", it?.latitude.toString())
-                locationTxt.text = it.latitude.toString() + "," + it.longitude
-            }
-        startLocationUpdate()
-    }
-
-    override fun onConnectionSuspended(p0: Int) {
-    }
-
-    override fun onConnectionFailed(p0: ConnectionResult) {
-    }
 
 }
+
+
