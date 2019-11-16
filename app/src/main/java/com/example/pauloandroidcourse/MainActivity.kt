07081de3942f0dec34aa5cbcb2b8e5768d827838
omar jakmira <poweromar19942@gmail.com>
2019-11-16 18:25:12 +0200

    simple fragment

--- app/src/main/java/com/example/pauloandroidcourse/MainActivity.kt
+++ app/src/main/java/com/example/pauloandroidcourse/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.pauloandroidcourse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var fmg = supportFragmentManager
+       // var fragment = fmg.findFragmentById(R.id.frame)
+        var fragment = MainFragment()
+        fmg.beginTransaction()
+            .add(R.id.frame,fragment)
+            .commit()
+
     }
 }
---
+++ app/src/main/java/com/example/pauloandroidcourse/MainFragment.kt
@@ -0,0 +1,19 @@
+package com.example.pauloandroidcourse
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container,false)
+    }
+}
