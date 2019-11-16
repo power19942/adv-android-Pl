c095c4f641136af01b29d27609593af54ef2c1a2
omar jakmira <poweromar19942@gmail.com>
2019-11-16 21:30:23 +0200

    pass data [not work]

--- app/src/main/java/com/example/pauloandroidcourse/ListFragment.kt
+++ app/src/main/java/com/example/pauloandroidcourse/ListFragment.kt
@@ -1,53 +1,82 @@
 package com.example.pauloandroidcourse
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.telecom.Call
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import android.widget.Toast
 import androidx.fragment.app.ListFragment
 import com.example.pauloandroidcourse.data.Course
+import java.lang.Exception
 
-class ListFragment : ListFragment() {
+class ListFragment : ListFragment(){
 
+    var courses: ArrayList<Course>? = null
+    var listener:CallBacks? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(activity,"",Toast.LENGTH_SHORT).show()
         var courses = ArrayList<Course>()
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        courses.add(Course("a course",0))
-        var adapter = CustomAdater(activity!!.applicationContext,R.layout.item_item,courses)
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        courses.add(Course("a course", 0))
+        var adapter = CustomAdater(activity!!.applicationContext, R.layout.item_item, courses)
         listAdapter = adapter
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container,false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
+
+        var course = courses?.get(position)
+       listener?.onItemSelected(course!!)
+
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is CallBacks){
+            listener = context
+        }else{
+            throw Exception("Shit")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface CallBacks {
+        fun onItemSelected(course: Course)
     }
 }
 
-interface CallBacks{
-    fun onItemSelected(course: Course)
-}
--- app/src/main/java/com/example/pauloandroidcourse/MainActivity.kt
+++ app/src/main/java/com/example/pauloandroidcourse/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.pauloandroidcourse
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.pauloandroidcourse.data.Course
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ListFragment.CallBacks {
+    
+    override fun onItemSelected(course: Course) {
+        
+        Toast.makeText(this@MainActivity,"clicked",Toast.LENGTH_SHORT).show()
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
