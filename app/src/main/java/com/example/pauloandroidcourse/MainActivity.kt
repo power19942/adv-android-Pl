be97fe62ad96c5ff413791f4afabc310dc1acfea
omar jakmira <poweromar19942@gmail.com>
2019-11-16 17:29:25 +0200

    simple animation

--- app/src/main/java/com/example/pauloandroidcourse/MainActivity.kt
+++ app/src/main/java/com/example/pauloandroidcourse/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.pauloandroidcourse
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var batAnim : AnimationDrawable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bat.setBackgroundResource(R.drawable.bat_anim)
+        batAnim = bat.background as AnimationDrawable
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        Toast.makeText(this@MainActivity,"touchec",Toast.LENGTH_SHORT).show()
+        batAnim.start()
+        return super.onTouchEvent(event)
     }
 }
